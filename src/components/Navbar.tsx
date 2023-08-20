1f27d75f9de5c8b1512a6ae71922d545bf1c0c64
nsola09 <>
2023-08-19 20:44:05 -0500

    auth working

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,26 @@
-import { getAuthSession } from '@/lib/nextauth'
-import React from 'react'
+import { getAuthSession } from "@/lib/nextauth";
+import Link from "next/link";
+import React from "react";
+import SignInButton from "./SignInButton";
 
-type Props = {}
+type Props = {};
 
-const Navbar = async(props: Props) => {
-    const session = await getAuthSession();
-  if(session?.user){
-    return <pre>{JSON.stringify(session.user, null, 2)}</pre> 
-  } else {
-    return <div>Not signed in</div>
-  }
-}
+const Navbar = async (props: Props) => {
+  return (
+    <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-10 h-fit border-zinc-300 py-2">
+      <div className="flex items-center justify-between h-full gap-2 px-8 mx-auto mx-w-7xl">
+        {/* Logo */}
+        <Link href="/" className="flex items-center gap-2">
+          <p className="rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold tansition-all hover:-translate-y-(2px) md-block dark:border-white">
+            Quizify
+          </p>
+        </Link>
+        <div className="flex items-center">
+            <SignInButton text="Sign In"/>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
